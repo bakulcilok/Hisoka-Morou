4d47d30361dc8acdc0ad4671407f2adb8392562c
bakulcilok <67635957+bakulcilok@users.noreply.github.com>
2022-06-09 12:45:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function startHisoka() {
     const hisoka = hisokaConnect({
         logger: pino({ level: 'silent' }),
         printQRInTerminal: true,
-        browser: ['Hisoka Multi Device','Safari','1.0.0'],
+        browser: ['Bakulcilok Multi Device','Safari','1.0.0'],
         auth: state
     })
 
